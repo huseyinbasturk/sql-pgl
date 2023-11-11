601ac76e03158b258d79151220350078827c08a3
huseyinbasturk <hb.basturk41@gmail.com>
2023-11-11 23:37:15 +0100

    sql injection

--- api/social-repo/src/repos/user-repo.js
+++ api/social-repo/src/repos/user-repo.js
@@ -8,25 +8,37 @@ class UserRepo{
         return toCamelCase(rows);
     }
 
-    static async findById(id){
-        //warning : really big security issue!
-        const {rows} = await pool.query(`
-            select * from users where id = ${id};
-        `);
+    static async findById(id) {
+        const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1;`, [
+          id
+        ]);
+    
+        return toCamelCase(rows)[0];
+      }
+
+    static async insert(username, bio){
+        const {rows } = await pool.query('insert into users (username, bio) values ($1, $2) returning *;',
+        [username,bio]
+        );
+
+        return toCamelCase(rows);
+    }
+
+    static async update(id, username, bio){
+        const {rows} = await pool.query(
+            'update users set username = $1, bio=$2 where id =$3 returning*;',
+            [username, bio, id]
+        );
 
         return toCamelCase(rows)[0];
 
     }
 
-    static async insert(){
-
-    }
-
-    static async update(){
-
-    }
-
-    static async delete(){
+    static async delete(id){
+        const {rows} = await pool.query(
+            'DELETE FROM users where id = $1 returning*;', [id]
+        );
+        return toCamelCase(rows)[0];
 
     }
 
